75d5aed7a12572940d414b7a5208e1067c3e66c0
Steven.Noriega <haroldnorieg4@gmail.com>
2025-12-04 09:34:00 -0500

    Creating initial file

---
+++ conftest.py
@@ -0,0 +1,25 @@
+import pytest
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--language",
+        action="store",
+        default=None
+    )
+
+@pytest.fixture(scope="function")
+def browser(request):
+    user_language = request.config.getoption("language")
+
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+
+    print(f"\nstart chrome browser for test with language= '{user_language}'")
+    browser = webdriver.Chrome(options=options)
+
+    yield browser
+    print('\nend chrome browser')
+    browser.quit()
